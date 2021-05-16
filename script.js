2179a9b83aaa2a00df38674c0f1651169ca0fd0d
Gustavo <gusbuss_1@outlook.com>
2021-05-15 20:57:05 -0500

    created if statements to determine the combos for password variations depending on what you select

--- script.js
+++ script.js
@@ -17,4 +17,47 @@ function writepassword() {
     var upperCase = confirm("do you want upper case letters in your password?");
     var numberConfirm = confirm("do you want numbers in your password?");
     var special = confirm('do you want special characters in your password? ex: "@, $, #, %"');
-    
+
+// create if statement to determine what happens when you click cetain settings for the password 
+
+if (lowercase === true && upperCase === false && numberConfirm === false && special === false){
+    generatePassword(lowerLetter);
+} else if (lowercase === false && upperCase === true && numberConfirm === false && special === false){
+     generatePassword(upperLetter);
+} else if (lowercase === false && upperCase === false && numberConfirm === false && special === true){
+    generatePassword(specialChar);
+} else if (lowercase === false && upperCase === false && numberConfirm === true && special === false){
+    generatePassword(numbers);
+} else if (lowercase === true && upperCase === true && numberConfirm === false && special === false)
+    var combo1 = lowerLetter.concat(upperLetter);
+    generatePassword(combo1);
+} else if (lowercase === true && upperCase === false && numberConfirm === false && special === true)
+var combo2 = lowerLetter.concat(specialChar);
+generatePassword(combo2);
+} else if (lowercase === true && upperCase === false && numberConfirm === true && special === false)
+var combo3 = lowerLetter.concat(numbers);
+generatePassword(combo3);
+} else if (lowercase === true && upperCase === true && numberConfirm === false && special === true)
+var combo4 = lowerLetter.concat(upperLetter, specialChar);
+generatePassword(combo4);
+} else if (lowercase === true && upperCase === true && numberConfirm === true && special === false)
+var combo5 = lowerLetter.concat(upperLetter, numbers);
+generatePassword(combo5);
+} else if (lowercase === true && upperCase === false && numberConfirm === true && special === true)
+var combo6 = lowerLetter.concat(numbers, specialChar);
+generatePassword(combo6);
+} else if (lowercase === true && upperCase === true && numberConfirm === true && special === true)
+var combo7 = lowerLetter.concat(upperLetter, numbers, specialChar);
+generatePassword(combo7);
+} else if (lowercase === false && upperCase === true && numberConfirm === true && special === false)
+var combo8 = upperLetters.concat(numbers);
+generatePassword(combo8);
+} else if (lowercase === false && upperCase === true && numberConfirm === false && special === true)
+var combo9 = upperLetter.concat(specialChar);
+generatePassword(combo9);
+} else if (lowercase === false && upperCase === true && numberConfirm === true && special === true)
+var combo10 = upperLetter.concat(numbers, specialChar);
+generatePassword(combo10);
+} else if (lowercase === false && upperCase === false && numberConfirm === true && special === true)
+var combo11 = numbers.concat(specialChar);
+generatePassword(combo11);
