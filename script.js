48eafe3d66396bea0deb79305d415f22d8786349
Gustavo <gusbuss_1@outlook.com>
2021-05-15 19:53:45 -0500

    changed the setup for the html file, I re did the functions in java

--- script.js
+++ script.js
@@ -1,72 +1,20 @@
-//dom elements
-var resultEl = document.getElementById("result");
-var lengthEl = document.getElementById("length");
-var uppercaseEl = document.getElementById("uppercase");
-var  lowercaseEl = document.getElementById("lowercase");
-var  numbersEl = document.getElementById("numbers");
-var generateEl = document.getElementById("generate");
-var clipboardEl = document.getElementById("clipboard");
+// assignment code
+var generateBtn = document.querySelector("#generate");
+var password = 0;
+var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", '"',"#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ";", ":", "[", "]", "{", "}", '|', "?", "<", ">", ",", ".", "~"];
 
-
-var randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol
-};
-// generate event listen
-generateEl.addEventListener("click", () => {
-    var length = +lengthEl.Value;
-    var hasLower = lowercaseEl.checked;
-    var hasUpper = uppercaseEl.checked;
-    var hasNumber = numbersEl.checked;
-    var hasSymbol = symbolsEL.checked;
-    
-   resultEl.innerText = generatePassword(
-       hasLower, 
-       hasUpper, 
-       hasNumber, 
-       hasSymbol
-  );
-});
-
-// generate password function 
-function generatePassword(lower, upper, symbol, length) {
-  
-    let generatePassword = "";
-    var typesCount = lower + upper + numer + symbol;
-    var typesArr =[{ lower },{ upper },{ number},{ symbol }].filter
-    (items => Object.values(item)[0]
-    );
-    if(typesCount ===0) {
-        return "";
+// writing the password to the password input
+function writepassword() {
+    if (passlength <8 || passlength > 128 || isNaN(passlength) ){
+        alert("please choose a number between 8 and 128!");
+        writepassword();
+        return;
     }
-    for(let i = 0; i < length; i += typesCount) {
-        typesArr.forEach(type => {
-          var funcName = Object.keys(type)[0];
-
-          generatedPassword += rendomFunc[funcName]();
-        });
-}
-    var finalPassword = generatedPassword.slice(0, length);
-   
-    return finalPassword;
-}
-
-
-
-//generate function
-funtion getRandomLower() {
-    return String.fromCharCode(Math.floor(MAth.random() * 26) + 97);
-}
-funtion getRandomUpper() {
-    return String.fromCharCode(Math.floor(MAth.random() * 26) + 65);
-}
-funtion getRandomNumber() {
-    return String.fromCharCode(Math.floor(MAth.random() * 10) + 48);
-}
-funtion getRandomSymbol() {
-    var symbol = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    return symbols[MAth.floor(MAth.random() * symbols.length)];
-}
-
+    var lowercase = confirm("do you want lowercase letters in your password?");
+    var upperCase = confirm("do you want upper case letters in your password?");
+    var numberConfirm = confirm("do you want numbers in your password?");
+    var special = confirm('do you want special characters in your password? ex: "@, $, #, %"');
+    
