68b5a55dedd0e799f5a5d60ef4d2ee01fb5ab118
Gustavo <gusbuss_1@outlook.com>
2021-05-15 23:36:31 -0500

    I filled in the read me with a screenshot and details of my work, I also fixed some spellcheck errors and cleaned up my work, I also changed the name of the text area id

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Password_Generator
 This is a password generator using java/html/css
+
+
+link to a screenshot of the website functioning  https://drive.google.com/file/d/170gB2TbMRWysS-IoHubVxgUg3tsFOMwQ/view
+
+// I started by creating variables for each category
+// then after that I created a function for the number variable to determine what would happen if a number was chosen that wasn't between the numbers given
+// I created an if statement to create combinations of actions when each was clicked
+// then I created a function to determine the length of the password 
+//then I made it to where the password generated is pasted to the text area
+// i finally added a event listener 
--- script.js
+++ script.js
@@ -1,80 +1,89 @@
 // assignment code
 var generateBtn = document.querySelector("#generate");
 var password = 0;
-var lowerLetter = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"];
-var upperLetter = ["A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"];
-var numbers = ["0, 1, 2, 3, 4, 5, 6, 7, 8, 9"];
-var specialChar = ["!", '"',"#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ";", ":", "[", "]", "{", "}", '|', "?", "<", ">", ",", ".", "~"];
+var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacter = ["!", '"',"#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ";", ":", "[", "]", "{", "}", "|", "?", "<", ">", ",", ".", "~"];
 
 // writing the password to the password input
 function writepassword() {
-    if (passlength <8 || passlength > 128 || isNaN(passlength) ){
-        alert("please choose a number between 8 and 128!");
+    passLength = parseInt(prompt("Password length between 8 - 128"), 8);
+    console.log(passLength);
+    if ( passLength < 8 || passLength > 128 || isNaN(passLength) ){
+        alert("please pick a number between 8 - 128");
         writepassword();
         return;
     }
-    var lowercase = confirm("do you want lowercase letters in your password?");
-    var upperCase = confirm("do you want upper case letters in your password?");
-    var numberConfirm = confirm("do you want numbers in your password?");
-    var special = confirm('do you want special characters in your password? ex: "@, $, #, %"');
+    var lowercase = confirm("Would you like Lowercase letters in your password?");
+    var upperCase = confirm("Would you like Upper case letters in your password?");
+    var numberConfirmation = confirm("would you like Numbers in your password?");
+    var special = confirm('would you like Special Characters in your password? ex: "@, $, #, %"');
 
 // create if statement to determine what happens when you click cetain settings for the password 
+// each if statement will take in a boolean of either lowercase, uppercase, numberconfirm and special
 
-if (lowercase === true && upperCase === false && numberConfirm === false && special === false){
+
+if (lowercase === true && upperCase === true && numberConfirmation === true && special === true){
+   var combanation1 = upperLetter.concat(lowerLetter, numbers, specialCharacter)
+    generatePassword(combanation1);
+} else if (lowercase === true && upperCase === true && numberConfirmation === true && special === false){
+    var combination2 = upperLetter.concat(lowerLetter, numbers)
+    generatePassword(combination2);
+} else if (lowercase === true && upperCase === true && numberConfirmation === false && special === false){
+    var combination3 = upperLetter.concat(lowerLetter)
+    generatePassword(combination3);
+} else if (lowercase === true && upperCase === false && numberConfirmation === false && special === false){
     generatePassword(lowerLetter);
-} else if (lowercase === false && upperCase === true && numberConfirm === false && special === false){
-     generatePassword(upperLetter);
-} else if (lowercase === false && upperCase === false && numberConfirm === false && special === true){
-    generatePassword(specialChar);
-} else if (lowercase === false && upperCase === false && numberConfirm === true && special === false){
-    generatePassword(numbers);
-} else if (lowercase === true && upperCase === true && numberConfirm === false && special === false){
-    var combo1 = lowerLetter.concat(upperLetter);
-    generatePassword(combo1);
-} else if (lowercase === true && upperCase === false && numberConfirm === false && special === true){
-    var combo2 = lowerLetter.concat(specialChar);
-    generatePassword(combo2);
-} else if (lowercase === true && upperCase === false && numberConfirm === true && special === false){
-var combo3 = lowerLetter.concat(numbers);
-generatePassword(combo3);
-} else if (lowercase === true && upperCase === true && numberConfirm === false && special === true){
-var combo4 = lowerLetter.concat(upperLetter, specialChar);
-generatePassword(combo4)
-} else if (lowercase === true && upperCase === true && numberConfirm === true && special === false){
-var combo5 = lowerLetter.concat(upperLetter, numbers);
-generatePassword(combo5);
-} else if (lowercase === true && upperCase === false && numberConfirm === true && special === true){
-var combo6 = lowerLetter.concat(numbers, specialChar);
-generatePassword(combo6);
-} else if (lowercase === true && upperCase === true && numberConfirm === true && special === true){
-var combo7 = lowerLetter.concat(upperLetter, numbers, specialChar);
-generatePassword(combo7);
-} else if (lowercase === false && upperCase === true && numberConfirm === true && special === false){
-var combo8 = upperLetters.concat(numbers);
-generatePassword(combo8);
-} else if (lowercase === false && upperCase === true && numberConfirm === false && special === true){
-var combo9 = upperLetter.concat(specialChar);
-generatePassword(combo9);
-} else if (lowercase === false && upperCase === true && numberConfirm === true && special === true){
-var combo10 = upperLetter.concat(numbers, specialChar);
-generatePassword(combo10);
-} else if (lowercase === false && upperCase === false && numberConfirm === true && special === true){
-var combo11 = numbers.concat(specialChar);
-generatePassword(combo11);
+} else if (lowercase === false && upperCase === true && numberConfirmation === false && special === false){
+    generatePassword(upperLetter);
+} else if (lowercase === false && upperCase === true && numberConfirmation === true && special === false){
+    var combination4 = upperCase.concat(numbers);
+    generatePassword(combination4);
+} else if (lowercase === false && upperCase === true && numberConfirmation === true && special === true){
+var combination5 = upperLetter.concat(numbers, specialCharacter);
+generatePassword(combination5);
+} else if (lowercase === false && upperCase === false && numberConfirmation === true && special === false){
+generatePassword(numberConfirmation)
+} else if (lowercase === false && upperCase === false && numberConfirmation === true && special === true){
+var combinatipon6 = numberConfirmation.concat(specialCharacter);
+generatePassword(combinatipon6);
+} else if (lowercase === false && upperCase === false && numberConfirmation === false && special === true){
+generatePassword(specialCharacter);
+} else if (lowercase === true && upperCase === false && numberConfirmation === false && special === true){
+var combination7 = lowerLetter.concat(specialCharacter);
+generatePassword(combination7);
+} else if (lowercase === false && upperCase === true && numberConfirmation === false && special === true){
+var combination8 = upperLetters.concat(specialCharacter);
+generatePassword(combination8);
+} else if (lowercase === false && upperCase === true && numberConfirmation === true && special === false){
+var combination9 = upperLetter.concat(numberConfirmation);
+generatePassword(combination9);
+} else if (lowercase === true && upperCase === false && numberConfirmation === true && special === false){
+var combination10 = lowerLetter.concat(numberConfirmation);
+generatePassword(combination10);
+} else if (lowercase === false && upperCase === false && numberConfirmation === false && special === false){
+alert("please pick one of the following");
+writepassword();
+return;
 };
 
 // creating a function for the password 
-
+/**
+ *  generatePassword will take in a combination from the above if statements
+ *  then will create the password with the specified length and combinations
+ * 
+ */
 function generatePassword(combination) {
     var generatePassword = [];
-     for(i = 0; i < passlength; i++){
-         var randomChar = combination[Math.floor(Math.random() * combination.length)];
-         generatePassword.push(randomChar);
+     for (i = 0; i < passLength; i++){
+         var randomCharacter = combination[Math.floor(Math.random() * combination.length)];
+         generatePassword.push(randomCharacter);
      }
-     password = generatePassword.join("");
+     password = generatePassword.join("");  //join will add all the parts of the password to the password string
 }
 var passwordText = document.querySelector("#password");
-passwordText.value = password;
+passwordText1.value = password;     //this is used to paste the code to text area 
 }
 
 
